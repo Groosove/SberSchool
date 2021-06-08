dc793b2e1f90c1fe9e5f53f7cd77a228626c5dc5
Groosove <arturianec2013@gmail.com>
2021-06-08 16:07:33 +0300

    Start write lesson-20 homework

---
+++ Lesson-20/TestPackage/TestPackage/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TestPackage
+//
+//  Created by Fenix Lavon on 6/8/21.
+//  Copyright © 2021 Fenix Lavon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ Lesson-20/TestPackage/TestPackage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestPackage
+//
+//  Created by Fenix Lavon on 6/8/21.
+//  Copyright © 2021 Fenix Lavon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
